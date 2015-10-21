bce6596df4680957b0ca23c28d93914b2b33110d
Fantomas42 <fantomas42@gmail.com>
2015-10-21 11:53:00 +0200

    Custom alphabet

--- zinnia_hashids/factory.py
+++ zinnia_hashids/factory.py
@@ -2,10 +2,11 @@
 from hashids import Hashids
 
 from zinnia_hashids.settings import HASHIDS_SALT
+from zinnia_hashids.settings import HASHIDS_ALPHABET
 from zinnia_hashids.settings import HASHIDS_MIN_LENGTH
-
 
 hashids = Hashids(
     salt=HASHIDS_SALT,
-    min_length=HASHIDS_MIN_LENGTH
+    min_length=HASHIDS_MIN_LENGTH,
+    alphabet=HASHIDS_ALPHABET or Hashids.ALPHABET
 )
--- zinnia_hashids/settings.py
+++ zinnia_hashids/settings.py
@@ -4,3 +4,5 @@ from django.conf import settings
 HASHIDS_SALT = getattr(settings, 'ZINNIA_HASHIDS_SALT', 'Zinnia rocks')
 
 HASHIDS_MIN_LENGTH = getattr(settings, 'ZINNIA_HASHIDS_MIN_LENGTH', 6)
+
+HASHIDS_ALPHABET = getattr(settings, 'ZINNIA_HASHIDS_ALPHABET', '')
