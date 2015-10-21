497ccd3b31563169dd7c3f0915199c38ef6b9580
Fantomas42 <fantomas42@gmail.com>
2015-10-21 16:54:59 +0200

    Better regexp handling the minimal size of the hash

--- zinnia_hashids/urls.py
+++ zinnia_hashids/urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import url
 
 from zinnia_hashids.views import EntryHashids
+from zinnia_hashids.settings import HASHIDS_MIN_LENGTH
 
 
 urlpatterns = [
-    url(r'^(?P<token>\w+)/$',
+    url(r'^(?P<token>\w{%i,})/$' % HASHIDS_MIN_LENGTH,
         EntryHashids.as_view(),
         name='entry_hashids'),
 ]
