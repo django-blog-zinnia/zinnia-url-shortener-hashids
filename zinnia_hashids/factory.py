b5e8165880b6893dabbee3cfd397aeadc3eceb4c
Fantomas42 <fantomas42@gmail.com>
2015-10-21 11:43:31 +0200

    Configurable minimal length of the hash

--- zinnia_hashids/factory.py
+++ zinnia_hashids/factory.py
@@ -2,8 +2,10 @@
 from hashids import Hashids
 
 from zinnia_hashids.settings import HASHIDS_SALT
+from zinnia_hashids.settings import HASHIDS_MIN_LENGTH
 
 
 hashids = Hashids(
-    salt=HASHIDS_SALT
+    salt=HASHIDS_SALT,
+    min_length=HASHIDS_MIN_LENGTH
 )
--- zinnia_hashids/settings.py
+++ zinnia_hashids/settings.py
@@ -2,3 +2,5 @@
 from django.conf import settings
 
 HASHIDS_SALT = getattr(settings, 'ZINNIA_HASHIDS_SALT', 'Zinnia rocks')
+
+HASHIDS_MIN_LENGTH = getattr(settings, 'ZINNIA_HASHIDS_MIN_LENGTH', 6)
