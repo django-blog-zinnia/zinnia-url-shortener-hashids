177fc7ef6fd6758eeb8234b1bfbd20d28bb9da4d
Fantomas42 <fantomas42@gmail.com>
2015-10-21 11:40:44 +0200

    Create a factory to centralize the configuration of Hashids constructor

--- zinnia_hashids/views.py
+++ zinnia_hashids/views.py
@@ -1,12 +1,10 @@
 """Views for Zinnia Hashids"""
-from hashids import Hashids
-
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import RedirectView
 
 from zinnia.models.entry import Entry
 
-from zinnia_hashids.settings import HASHIDS_SALT
+from zinnia_hashids.factory import hashids
 
 
 class EntryHashids(RedirectView):
@@ -22,7 +20,6 @@ class EntryHashids(RedirectView):
         in the 'token' variable and return the get_absolute_url
         of the entry.
         """
-        hashids = Hashids(salt=HASHIDS_SALT)
         unhashed_pk = hashids.decode(kwargs['token'])[0]
         entry = get_object_or_404(Entry.published, pk=unhashed_pk)
         return entry.get_absolute_url()
